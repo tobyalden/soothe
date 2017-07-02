2d3ce63e191c88c9ceaa800270560d79a842a627
Toby Alden <toby.alden@gmail.com>
2017-07-02 09:01:08 -0700

    Add timers

---
+++ src/entities/Timer.hx
@@ -0,0 +1,44 @@
+package entities;
+
+class Timer
+{
+
+  static var allTimers:Array<Timer> = new Array<Timer>();
+
+  public var count:Int;
+  public var duration:Int;
+
+  public function new(duration:Int)
+  {
+    this.duration = duration;
+    count = 0;
+    allTimers.push(this);
+  }
+
+  public function restart()
+  {
+    count = duration;
+  }
+
+  public function percentComplete() {
+    return count / duration;
+  }
+
+  public function isActive()
+  {
+    return count > 0;
+  }
+
+  public static function updateAll()
+  {
+    for(timer in allTimers)
+    {
+      /*trace(timer.count);*/
+      if(timer.count > 0)
+      {
+        timer.count -= 1;
+      }
+    }
+  }
+
+}
