42981c7977f25b9593e630a00f7067970b4c197c
Toby Alden <toby.alden@gmail.com>
2017-07-15 09:44:55 -0700

    Add group behavior for lusters

--- src/entities/Luster.hx
+++ src/entities/Luster.hx
@@ -14,9 +14,14 @@ class Luster extends ActiveEntity
     public static inline var MISSILE_SPEED = 6;
     public static inline var SHOT_COOLDOWN = 20;
 
+    public static inline var GROUP_SPACING = 40;
+
+    private static var groupLogicApplied:Bool = false;
+
     public var bobTimer:Float;
     public var destination:Point;
     private var cooldownTimer:Int;
+
 
     public function new(x:Int, y:Int)
     {
@@ -35,12 +40,35 @@ class Luster extends ActiveEntity
         finishInitializing();
     }
 
+    public function offsetDestinationForGroup()
+    {
+      // later factor in distance & make it only run once per frame
+      var allLusters = new Array<Luster>();
+      scene.getClass(Luster, allLusters);
+      var count = 0;
+      for(luster in allLusters) {
+        if(count == 0) {
+          // do nothing
+        }
+        else if(count % 2 == 0) {
+          luster.destination.x += Math.ceil(count/2) * GROUP_SPACING;
+          luster.destination.y -= Math.ceil(count/2) * GROUP_SPACING;
+        }
+        else {
+          luster.destination.x += Math.ceil(count/2) * -GROUP_SPACING;
+          luster.destination.y -= Math.ceil(count/2) * GROUP_SPACING;
+        }
+        count++;
+      }
+    }
+
     public override function update()
     {
       var player = HXP.scene.getInstance("player1");
       destination.x = player.centerX - halfWidth;
       destination.y = player.centerY - halfHeight - HOVER_HEIGHT;
-      moveTowards(destination.x, destination.y, CHASE_SPEED, "enemy");
+      offsetDestinationForGroup();
+      moveTowards(destination.x, destination.y, CHASE_SPEED);
       if(distanceToPoint(destination.x, destination.y, true) < 50) {
         sprite.play("shoot");
         shoot();
@@ -58,7 +86,7 @@ class Luster extends ActiveEntity
       }
       var bullet = collide("bullet", x, y);
       if(bullet != null) {
-        takeDamage(10);
+        takeDamage(50);
         scene.remove(bullet);
       }
       super.update();
